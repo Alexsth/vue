f2605f357d96f3f527cfa5cac237f7b54de8a815
AlexSth <alexshrestha77@gmail.com>
2020-09-25 15:44:18 +0545

    Computed properties completed

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name:'',
-        age:''
+        age:20,
+        a:0,
+        b:0
     },
     methods: {
      
+    },
+    computed:{
+        addtoA: function(){
+            return this.age + this.a;
+        },
+        addtoB: function(){
+            return this.age + this.b;
+        }
     }
 })
