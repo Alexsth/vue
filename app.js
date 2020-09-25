cf0792b6b38f823bf8210017ba7574df49d6595c
AlexSth <alexshrestha77@gmail.com>
2020-09-25 14:51:05 +0545

    Keyboard events done

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25 ,
-        x : 0,
-        y:0
-
+   
     },
     methods: {
-        add: function(x){
-           this.age+= x;
-        },
-        subtract:function(y){
-            this.age-=y;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('you clicked it');
-        }
+       logName: function(){
+           console.log('You entered your name');
+       },
+       logAge: function(){
+        console.log('You entered your age');
+    }
     }
 })
